d20b359b3a53ab45c7b4eaeb8ec2575eddf033fd
briankaboyi <brianmkaboyi@gmail.com>
2024-02-05 10:54:32 +0300

    fixed chat widget

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -4,104 +4,93 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 
-class ChatWidget extends GetView{
+class ChatWidget extends GetView {
   final Image image;
   final String text;
   final String time;
   final String age;
   final String message;
   @override
-  const ChatWidget(  {required this.image, required this.text, required this.age,required this.time ,required this.message });
+  const ChatWidget(
+      {required this.image,
+      required this.text,
+      required this.age,
+      required this.time,
+      required this.message});
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      width: Get.width,
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Row(children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: Column(
-              children: [
-                Container(
-
-                    width: 50,
-                    height: 50,
-                    child: ClipOval(
-                      child: image,
-
-                    )
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-            
-              crossAxisAlignment: CrossAxisAlignment.start,
-            
-              children: [
-                Row(
-            
+        width: Get.width,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: Column(
                   children: [
-                    Column(
+                    Container(
+                        width: 50,
+                        height: 50,
+                        child: ClipOval(
+                          child: image,
+                        )),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.all(3.0),
-                          child: TextWidget(
-                            value: '$text, $age',
-                            textAlign: TextAlign.start,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-
-                          ),
-          )
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(3.0),
+                              child: TextWidget(
+                                value: '$text, $age',
+                                textAlign: TextAlign.start,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
+                        ),
+                        Expanded(
+                          child: Container(),
+                        ),
+                        Column(children: [
+                          Padding(
+                              padding: const EdgeInsets.all(3.0),
+                              child: TextWidget(
+                                value: time,
+                                textAlign: TextAlign.start,
+                                fontWeight: FontWeight.w300,
+                                fontSize: 13,
+                              ))
+                        ]),
                       ],
                     ),
-                    Expanded(child: Container(),),
-                    Column(
+                    Row(
                       children: [
                         Padding(
                           padding: const EdgeInsets.all(3.0),
                           child: TextWidget(
-                            value: time,
+                            value: message,
                             textAlign: TextAlign.start,
                             fontWeight: FontWeight.w300,
-          fontSize: 16,)
-                        )
-                          ]
+                            fontSize: 13,
                           ),
-
+                        ),
                       ],
                     ),
                   ],
                 ),
+              ),
+
+            ],
           ),
-          Row(
-                  children: [
-            
-                    Padding(
-                      padding: const EdgeInsets.all(3.0),
-                      child: TextWidget(
-                        value:message,
-                        textAlign: TextAlign.start,
-
-                        fontWeight: FontWeight.w300, fontSize: 13,
-
-                      ),
-                    ),
-            
-                  ],
-                ),
-              ],
-            ),
-          )
-    );
-
-
-
-
-
-
+        ));
   }
 }
