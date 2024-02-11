2c8c0784d82fdc278525712a62ab3516bd09b43c
briankaboyi <brianmkaboyi@gmail.com>
2024-02-11 15:05:38 +0300

    working lazy load

--- lib/other_pages/Teams.dart
+++ lib/other_pages/Teams.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class Teams extends GetxController {
@@ -10,6 +11,11 @@ class Teams extends GetxController {
   RxList jsonList = []
       .obs; //create a reactive list jsonList. reactive list allow reactive programming and automatic UI updates when the list changes. .obs allows you to listen to changes in the list and react accordingly.
 
+  RxInt teamId = 0.obs;
+  RxList teamList = [].obs;
+
+  void testid()=> print(teamId);
+
   Future<Teams> getTeams() async {
     //asynchronous method that returns a future of type Teams
     var response = await dio.get(
@@ -19,4 +25,16 @@ class Teams extends GetxController {
     print(jsonList); //json list is printed to the console
     return this; //returns the current instance of the Teams class|| not necessary if you dont plan to chain methods or perform any additional operations
   }
+
+  Future<Teams> getTeamById(teamId) async{
+    var response = await dio.get( 'https://www.balldontlie.io/api/v1/teams/$teamId' );
+    teamList.value = response.data[
+        'data'];
+    return this;
+}
+
+  RxInt visibleItems = 10.obs;//the number of visible items on loading of this page. obs makes it observable as the value is meant to change later in the code
+  ScrollController scrollController = ScrollController();//instance of the ScrollController used for handling scroll behavior
+
+
 }
--- lib/other_pages/nba_data.dart
+++ lib/other_pages/nba_data.dart
@@ -6,73 +6,86 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 
-class NbaData extends GetView<Teams> {//nbadata can access functionality provided by the teams controller through the Get package.
+class NbaData extends GetView<Teams> {
+  //nbadata can access functionality provided by the teams controller through the Get package.
   @override
   Widget build(BuildContext context) {
-    RxInt _visibleItems = 10.obs;
-    ScrollController _scrollController = ScrollController();
-
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {
-        _visibleItems.value += 10;
-      }});
+    controller.scrollController.addListener(() {
+      //adds a listener to the ScrollController instance
+      if (controller.scrollController.position.pixels ==
+          controller.scrollController.position.maxScrollExtent) {
+        //the the user reaches the end  of scrollable items, the value of vivsible items is increased by 10 after ta delay of two seconds
+        Future.delayed(const Duration(seconds: 2), () {
+          controller.visibleItems.value += 10;
+        });
+      }
+      ;
+    });
     // TODO: implement build
     return Scaffold(
       body: NotificationListener<ScrollNotification>(
-        onNotification: (ScrollNotification scrollInfo){
+        onNotification: (ScrollNotification scrollInfo) {
           return true;
         },
-        child: FutureBuilder<Teams>(//future builder handles asynchronous data loading
-          future: controller.getTeams(),//getteams is a method in the teams class that returns a future<Teams>
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {//builder takes a callback with two parameters. buildcontext and snapshot.
-            if (snapshot.connectionState == ConnectionState.done) {//the connection state of the snapshot is checked , if done , it returns a list view
-              return ListView.builder(
-                controller: _scrollController,
-                itemCount: _visibleItems.value,
-                itemBuilder: (BuildContext context, int index) {
-                  if (index== _visibleItems.value-1){
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CircularProgressIndicator(),
-                      ],
-                    );
-                  }
-                 else if (index < controller.jsonList.length){//creates a list of listTile  widgets based on jsonList length
-                   var e = controller.jsonList[index];
-                   return Padding(
-                     padding: const EdgeInsets.all(20.0),
-                     child: Container(
-                       height: 60,
-                       color: Colors.black12,
-                       child: ListTile(
-                         title: Text(e['full_name'])//each tile contains the value of e['full_name'] from the json list from the corresponding index indicated by e.
+        child: FutureBuilder<Teams>(
+          //future builder handles asynchronous data loading
+          future: controller
+              .getTeams(), //getteams is a method in the teams class that returns a future<Teams>
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            //builder takes a callback with two parameters. buildcontext and snapshot.
+            if (snapshot.connectionState == ConnectionState.done) {
+              //the connection state of the snapshot is checked , if done , it returns a list view
+              return Obx(() => ListView.builder(
+                    controller: controller.scrollController,
+                    itemCount: controller.visibleItems.value,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index == controller.visibleItems.value - 1) {
+                        if (controller.visibleItems.value <
+                            controller.jsonList.length) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        } else {
+                          return const SizedBox
+                              .shrink(); // displays when the index is out of range. prevents an  out of range error
+                        }
+                      } else if (index < controller.jsonList.length) {
+                        //creates a list of listTile  widgets based on jsonList length
+                        var e = controller.jsonList[index];
 
-                       ),
-                     ),
-                   );
-                 }else{
-                   return SizedBox.shrink();// displays when the index is out of range. prevents an  out of range error
-                 }
+                        return Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: InkWell(
+                            onTap: () {
+                              controller.teamId.value = controller.jsonList[index]['id'];
+                              controller.testid();
+                            },
+                            child: Container(
+                              height: 60,
+                              color: Colors.black12,
+                              child: ListTile(
+                                  title: Text(e[
+                                      'full_name']) //each tile contains the value of e['full_name'] from the json list from the corresponding index indicated by e.
 
-
-                },
-
-
-
-              );
-            }else{
-              return Center(
-                child: CircularProgressIndicator(), //if the connection state is not done,  a circular progress indicator is displayed at the center of the screen
+                                  ),
+                            ),
+                          ),
+                        );
+                      } else {
+                        return const SizedBox
+                            .shrink(); // displays when the index is out of range. prevents an  out of range error
+                      }
+                    },
+                  ));
+            } else {
+              return const Center(
+                child:
+                    CircularProgressIndicator(), //if the connection state is not done,  a circular progress indicator is displayed at the center of the screen
               );
             }
-
-
           },
         ),
       ),
     );
   }
-
 }
